f5378c9877d5496717801756093c9ee5c0f56d8d
Malak Parmar <malakparmar.29@gmail.com>
2025-08-01 17:37:19 -0400

    added lambda function

--- src/main/java/com/iam/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/iam/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.iam.common.exception;
 
 import com.iam.common.response.ApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,7 +38,7 @@ public class GlobalExceptionHandler {
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(error -> error.getDefaultMessage())
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
         return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST)
